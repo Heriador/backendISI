946102c0340b86caaf4a1593e8e06539a5834b33
Angel Miranda <mirandaangel83@gmail.com>
2020-07-29 08:31:21 -0500

    esperemos

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class Sidebar extends Component {
+  state = {
+  };
+
+  toggleMenuState(menuState) {
+    if (this.state[menuState]) {
+      this.setState({[menuState] : false});
+    } else if(Object.keys(this.state).length === 0) {
+      this.setState({[menuState] : true});
+    } else {
+      Object.keys(this.state).forEach(i => {
+        this.setState({[i]: false});
+      });
+      this.setState({[menuState] : true});
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      this.onRouteChanged();
+    }
+  }
+
+  onRouteChanged() {
+    document.querySelector('#sidebar').classList.remove('active');
+    Object.keys(this.state).forEach(i => {
+      this.setState({[i]: false});
+    });
+  } 
+
+  render () {
+    return (
+      <nav className="sidebar sidebar-offcanvas" id="sidebar">
+        <ul className="nav mt-4">
+        <li className={ this.isPathActive('/AddFiles') ? 'nav-item active' : 'nav-item' }>
+            <Link className="nav-link" to="/addFiles">
+              <i className="mdi mdi-television menu-icon"></i>
+              <span className="menu-title">Add Files</span>
+            </Link>
+          </li>
+          <li className={ this.isPathActive('/showFiles') ? 'nav-item active' : 'nav-item' }>
+            <Link className="nav-link" to="/showFiles">
+              <i className="mdi mdi-television menu-icon"></i>
+              <span className="menu-title">Show Files</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+
+  isPathActive(path) {
+    return this.props.location.pathname.startsWith(path);
+  }
+
+  componentDidMount() {
+    this.onRouteChanged();
+    // add className 'hover-open' to sidebar navitem while hover in sidebar-icon-only menu
+    const body = document.querySelector('body');
+    document.querySelectorAll('.sidebar .nav-item').forEach((el) => {
+      
+      el.addEventListener('mouseover', function() {
+        if(body.classList.contains('sidebar-icon-only')) {
+          el.classList.add('hover-open');
+        }
+      });
+      el.addEventListener('mouseout', function() {
+        if(body.classList.contains('sidebar-icon-only')) {
+          el.classList.remove('hover-open');
+        }
+      });
+    });
+  }
+
+}
+
+export default withRouter(Sidebar);
